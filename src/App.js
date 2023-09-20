e74390372453e9e3da66070f09ec088511745619
Pablo Castro <pablocas-tro1@outlook.com>
2023-09-20 16:36:28 -0400

    Create a header and a nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
+function Header() {
+  return <h1>P&M Spa</h1>;
+}
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>Services</li>
+        <li>Calender</li>
+      </ul>
+    </nav>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
