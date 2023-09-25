157fa740cfd0e1ddd623b4e97579d110b34e5ba8
Pablo Castro <pablocas-tro1@outlook.com>
2023-09-25 15:03:49 -0400

    create a table and use props to display information on the calendar page

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,7 +1,49 @@
-function Calendar() {
+function Calendar({ tableinformation }) {
+  const { date, client, time, esthetician } = tableinformation[0];
+
   return (
-    <div>
-      <h1>Calendar Page</h1>
+    <div className="calendar-container">
+      <h1>Calander</h1>
+      <div className="calendar-details">
+        <table className="table-information">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Client</th>
+              <th>Time</th>
+              <th>esthetician</th>
+              <th>Edit</th>
+              <th>Cancel</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{date}</td>
+              <td>{client}</td>
+              <td>{time}</td>
+              <td>{esthetician}</td>
+              <td>
+                <button>reschedule</button>
+              </td>
+              <td>
+                <button>cancel</button>
+              </td>
+            </tr>
+            <tr>
+              <td>{date}</td>
+              <td>{client}</td>
+              <td>{time}</td>
+              <td>{esthetician}</td>
+              <td>
+                <button>reschedule</button>
+              </td>
+              <td>
+                <button>cancel</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
