2c901797f1d959a384fa8b7cf6104b10ca20bbb0
Pablo Castro <pablocas-tro1@outlook.com>
2023-09-26 17:23:44 -0400

    give a blur effect to the navbar when scrolling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import NavBar from "./NavBar";
+import { useEffect, useState } from "react";
 import "../stylesheets/header.css";
 import logo from "../media/RELUXE-LOGO.png";
 
 function Header() {
+  const [y, setY] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => {
+      setY(window.scrollY);
+    });
+  }, []);
+
   return (
-    <div className="header">
+    <div className={y < 50 ? "header" : "header blurry-header"}>
       <img className="logo" src={logo} alt="logo" />
       <NavBar />
     </div>
