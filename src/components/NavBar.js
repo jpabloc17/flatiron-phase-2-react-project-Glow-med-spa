11001b2cf30b2f8599bb4db5de911f5df2d51ce8
Pablo Castro <pablocas-tro1@outlook.com>
2023-10-03 17:33:59 -0400

    add some responsive features

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,26 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "../stylesheets/navBar.css";
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="nav-bar">
-      <ul>
+      <div
+        className="menu"
+        onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul
+        className={menuOpen ? "open" : ""}
+        onClick={() => {
+          setMenuOpen(false);
+        }}
+      >
         <li>
           <Link to="/">Home</Link>
         </li>
